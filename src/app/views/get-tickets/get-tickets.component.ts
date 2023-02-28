de733ad1c5d1ebb613b0c0176858888ea97535da
Maximiliano Escobar <emaxees@gmail.com>
2023-02-28 18:37:37 -0300

    feat: complete ui flow for buy tickets

--- src/app/views/get-tickets/get-tickets.component.ts
+++ src/app/views/get-tickets/get-tickets.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import { filter, last, map, mergeAll, Observable, take, takeWhile, timer } from 'rxjs';
+import { last, map, mergeAll, Observable, take, takeWhile, timer } from 'rxjs';
 import { MovieService } from 'src/app/services/movies/movies.service';
 import { ShowingsService } from 'src/app/services/showings/showings-service.service';
 import { TicketTypesService } from 'src/app/services/ticket-types/ticket-types.service';
@@ -22,6 +23,11 @@ export interface Row {
   seats: Seat[];
 }
 
+export interface Tab {
+  id: number;
+  name: string;
+}
+
 const counter = timer(0, 1000);
 
 @Component({
@@ -30,24 +36,42 @@ const counter = timer(0, 1000);
   styleUrls: ['./get-tickets.component.scss']
 })
 export class GetTicketsComponent implements OnInit, OnDestroy {
+  private sub: any;
   id: number = 0;
   movie$: Observable<Movie>;
   loading$: Observable<boolean>;
   showings$: Observable<Showing[]>;
   showing$: Observable<Showing>;
   tickets$: Observable<TicketType[]>
+  form: FormGroup;
+  tabs: Tab[] = [
+    {
+      id: 1,
+      name: 'Movie Info, tickets and seats',
+    },
+    {
+      id: 2,
+      name: 'Resume',
+    },
+    {
+      id: 3,
+      name: 'Pay',
+    },
+  ];
   timeRemaining$ = timer(0, 1000).pipe(
     map(n => (600 - n) * 1000),
     takeWhile(n => n >= 0),
   );
-  
-  private sub: any;
+  selectedTab: number = 1;
+  ticketType: number = 0;
+  seats: string[] = [];
 
   constructor(
     private route: ActivatedRoute, 
     private movieService: MovieService, 
     private showingsService: ShowingsService,
     private ticketTypeService: TicketTypesService,
+    private fb: FormBuilder
   ) {
     this.movie$ = movieService.entities$.pipe(mergeAll(), take(1), last());
     this.showings$ = showingsService.entities$;
@@ -59,6 +83,12 @@ export class GetTicketsComponent implements OnInit, OnDestroy {
       take(1), 
       last()
     );
+    this.form = this.fb.group({
+      email: ["", Validators.email],
+      phone: ["", [Validators.required, Validators.maxLength(9)]],
+      discountCode: ["", Validators.required],
+      creditcardNumber: ["", [Validators.required, Validators.maxLength(9)]],
+    });
   }
 
   ngOnInit() {
@@ -73,7 +103,24 @@ export class GetTicketsComponent implements OnInit, OnDestroy {
     this.sub.unsubscribe();
   }
 
-  handleSelectionChage($event: string[]) {
-    console.log($event);
+  handleSelectionChage(seats: any) {
+    this.seats = seats;
+  }
+
+  handleChage($event: any) {
+    const id = $event.target.value;
+    this.ticketType = Number(id);
+  }
+
+  handleClick() {
+    this.selectedTab = 2;
+  }
+
+  onSubmit() {
+    this.selectedTab = 3;
+  }
+
+  onSubmitCreditCard() {
+    window.open(`https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=testingworkflow`)
   }
 }
